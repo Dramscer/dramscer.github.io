888cb65ac5ba795f695ecada253aff59531047e0
Gabriel Garcia <gabrielgc.93@gmail.com>
2026-07-01 20:20:11 -0600

    Feat: Migración completa a Astro dinámico con Firebase y animaciones

---
+++ src/lib/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAslYO_FVuPnDezZN2_yMczDVU0gdpJua4",
+  authDomain: "portafolio-10805.firebaseapp.com",
+  projectId: "portafolio-10805",
+  storageBucket: "portafolio-10805.firebasestorage.app",
+  messagingSenderId: "1078012840052",
+  appId: "1:1078012840052:web:e74efabb107337144b6921"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
